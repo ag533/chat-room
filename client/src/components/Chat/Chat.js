b8dce7f8cab33a79482c7a514e14c01893effd02
Abhishek <abhisekgahlawatoo7@gmail.com>
2020-04-12 15:24:30 +0100

    Adding textContainer for online userList.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -6,12 +6,14 @@ import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
 const Chat = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
@@ -33,13 +35,18 @@ const Chat = () => {
 
             socket.off();
         }
-    }, [ENDPOINT, window.location.search]);
+    }, [ENDPOINT]);
 
     useEffect(() =>{
         socket.on('message', (message) => {
             setMessages([...messages, message]);
-        })
-    }, [messages]);
+        });
+
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+          });
+      
+    });
 
     const sendMessage = (event) => {
         event.preventDefault();
@@ -49,8 +56,6 @@ const Chat = () => {
         }
     }
 
-    console.log(message,messages);
-
     return(
         <div className="outerContainer">
             <div className="container">
@@ -58,6 +63,7 @@ const Chat = () => {
                 <Messages messages={messages} name={name}/>
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
             </div>
+            <TextContainer users={users}/>
         </div>
     )
 };
